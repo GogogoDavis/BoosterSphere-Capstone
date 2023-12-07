3cd71935ded0b39aec3022f2ec5966183a88f8cd
GogogoDavis <burkedavis@yahoo.com>
2023-12-07 14:23:30 -0700

    added initial seed

--- backend/migrations/20231207201515_initial_table.js
+++ backend/migrations/20231207201515_initial_table.js
@@ -3,7 +3,11 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('initial_table', table => {
+    table.increments('id');
+    table.text('name', 50);
+  })
+
 };
 
 /**
@@ -11,5 +15,6 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('initial_table');
   
 };
