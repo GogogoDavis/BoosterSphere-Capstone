978be4dbaeca2d215c11ffe8ae8593e0faef32f6
GogogoDavis <burkedavis@yahoo.com>
2023-12-08 10:47:01 -0700

    event updates

--- boostersphere/src/Events/Events.js
+++ boostersphere/src/Events/Events.js
@@ -4,13 +4,28 @@ import { Logout } from '../Logout/Logout';
 import React, { useState, useEffect } from 'react'; 
 import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
 import { DateTimePicker, DatePicker, LocalizationProvider, DateCalendar, DateRangeCalendar } from '@mui/x-date-pickers-pro';
+// import daMopey from '../DaMopester.jpg';
 
 export const Events = () => {
   const [value, setValue] = useState();
+  const [toggleAddEvent, setToggleAddEvent ] = useState(false); 
+
+  const toggleAdd = () => {
+    setToggleAddEvent(true); 
+  }
+
+  const toggleOff = () => {
+    setToggleAddEvent(false); 
+  }
+
+  const addEventWindow = () => {
+  
+  }
 
  
 return (
   <>
+  {!toggleAddEvent ? (
   <LocalizationProvider dateAdapter={AdapterDayjs}> 
     <div className="nav">
       <div className="links">
@@ -24,14 +39,22 @@ return (
       <Logout />
     </div>
     Events Page
+    <button id='freshAddBtn' onClick={toggleAdd}>Add Super Cool and Fresh New Event!</button>
+  </LocalizationProvider>
+  ): 
+  <LocalizationProvider dateAdapter={AdapterDayjs}>
+  <div>
+    <button onClick={toggleOff}>Return</button>
+  <form label='Add Event Form'>
     <div label='controlled calendar'>
       <DateRangeCalendar />
-    </div>
+      </div>
+  </form>
+  </div>
   </LocalizationProvider>
+  }
   </>
 );    
-
-
 }
 
 
