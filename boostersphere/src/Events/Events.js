0ed7d4e2ca66ecf0692e0300af18a70f02731400
GogogoDavis <burkedavis@yahoo.com>
2023-12-08 10:19:25 -0700

    figuring out calender jazz

--- boostersphere/src/Events/Events.js
+++ boostersphere/src/Events/Events.js
@@ -1,24 +1,45 @@
 import './Events.css'
 import { Link } from 'react-router-dom';
 import { Logout } from '../Logout/Logout';
+import React, { useState, useEffect } from 'react'; 
+import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
+import { DateTimePicker, DatePicker, LocalizationProvider, DateCalendar, DateRangeCalendar } from '@mui/x-date-pickers-pro';
 
 export const Events = () => {
+  const [value, setValue] = useState();
 
  
 return (
-<>
-<div className="nav">
-        <div className='links'>
-          <Link to='/Home' className='NavBar'>Home</Link>
-          <Link to='/Events' className='NavBar'>Events</Link>
-        </div>
-        <Logout />
+  <>
+  <LocalizationProvider dateAdapter={AdapterDayjs}> 
+    <div className="nav">
+      <div className="links">
+        <Link to="/Home" className="NavBar">
+          Home
+        </Link>
+        <Link to="/Events" className="NavBar">
+          Events
+        </Link>
       </div>
-
-Events Page
-
-</>
-)    
+      <Logout />
+    </div>
+    Events Page
+    <div label='controlled calendar'>
+      <DateRangeCalendar />
+    </div>
+  </LocalizationProvider>
+  </>
+);    
 
 
 }
+
+
+
+
+{/* <DemoItem label="Events Calender">
+      <DataRangeCalender
+        value={value}
+        onChange={(newValue) => setValue(newValue)}
+      />
+    </DemoItem> */}
