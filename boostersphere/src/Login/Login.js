2c9fdeb041404c6483913c828a8fc776a1220f46
dpark93 <d.park.official@gmail.com>
2023-12-08 13:22:39 -0700

    Register Function fixed with Image Upload function

--- boostersphere/src/Login/Login.js
+++ boostersphere/src/Login/Login.js
@@ -5,6 +5,9 @@ import {auth} from "../firebase"
 import {Link, useNavigate } from 'react-router-dom' ;
 import { AuthContext } from '../context/AuthContext';
 import { userContext } from '../App';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '../firebase';
+
 
 
 export const Login = () => {
@@ -16,11 +19,28 @@ export const Login = () => {
 
   const {dispatch} = useContext(AuthContext)
   const {setUserdata,  setThisuser} = useContext(userContext)
+  const {fulluserData, setFullUserData} = useContext(userContext)
 
 
   useEffect(()=>{
     dispatch({type:"LOGOUT"})
+
+    const fetchData = async () =>{
+      let list = []
+      try{
+        const querySnapshot = await getDocs(collection(db, "users"));
+        querySnapshot.forEach((doc) => {
+          list.push({id: doc. id, ...doc.data()});
+        });
+        setFullUserData(list)
+      } catch(err){
+        console.log(err);
+      }
+    };
+    fetchData()
   },[])
+  console.log(fulluserData)
+
 
   const handleLogin = (e) =>{
     e.preventDefault();
@@ -29,10 +49,10 @@ export const Login = () => {
     .then((userCredential) => {
       // Signed up 
       const user = userCredential.user;
-      setUserdata(user)
-      setThisuser(user.displayName)
+ 
       dispatch({type:"LOGIN", payload:user})
     //setisAdmin()
+    setUserdata(user)
       navigate("/Home")
       console.log(user)
       // ...
--- boostersphere/src/context/AuthContext.js
+++ boostersphere/src/context/AuthContext.js
@@ -20,3 +20,4 @@ export const AuthContextProvider = ({ children }) => {
         </AuthContext.Provider>
     );
 };
+
