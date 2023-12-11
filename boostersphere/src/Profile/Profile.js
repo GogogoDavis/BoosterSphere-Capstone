57414ab29505aca9c7c290ae50bcfa8bbc31cb47
dpark93 <d.park.official@gmail.com>
2023-12-11 12:58:21 -0700

    sidebar complete

---
+++ boostersphere/src/Profile/Profile.js
@@ -0,0 +1,14 @@
+
+import { Sidebar } from "../Sidebar/Sidebar"
+import './Profile.css'
+export const Profile = () =>{
+
+    return(
+<>
+<div className="Container">
+        <Sidebar />
+        <h1>MainPage</h1>
+</div>        
+</>        
+    )
+}
