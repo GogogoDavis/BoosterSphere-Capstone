8a4810297625f2d1c17adfccd9eb4ada70684926
GogogoDavis <burkedavis@yahoo.com>
2023-12-15 12:08:47 -0700

    added patch to event/details

--- boostersphere/src/Details/Details.js
+++ boostersphere/src/Details/Details.js
@@ -1,9 +1,22 @@
 import "./Details.css";
 import { useNavigate, useParams } from "react-router-dom";
+import DatePicker from "react-datepicker";
+import React, {useState} from 'react';
 
 export const Details = ({ item }) => {
   const { id } = useParams();
   const navigate = useNavigate();
+  const [editDetails, setEditDetails] = useState({
+    id: id,
+    title: "",
+    type: "",
+    description: "",
+    start: new Date,
+    end: new Date, 
+    fundRequired: 0,
+    volunteerNeeded: 0,
+    userId: 0
+  })
 
   function handleDeleteEvent() {
     // Fetch to sevrver but it is not getting the request
@@ -19,7 +32,7 @@ export const Details = ({ item }) => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(item),
+      body: JSON.stringify(editDetails),
     })
       .then(() => {
         console.log("Event updated successfully");
@@ -33,29 +46,128 @@ export const Details = ({ item }) => {
     navigate(`/events`);
   };
 
-  console.log(item);
+  // console.log(item);
+  console.log(editDetails);
 
   return (
     <container>
-  <div id='wrap'>
-    <ul>
-      <h1 style={{display: 'flex', justifyContent: 'center', color: 'salmon'}}>Details</h1>
-      <li className='addWrap'>{item.id}</li>
-      <li className='addWrap'>{item.title}</li>
-      <li className='addWrap'>{item.description}</li>
-      <li className='addWrap'>{item.start}</li>
-      <li className='addWrap'>{item.end}</li>
-      <li className='addWrap'>{item.fundRequired}</li>
-      <li className='addWrap'>{item.volunteerNeeded}</li>
-    </ul> 
-        <div style={{margin: '10px'}}> <button className= 'detailBtn' onClick={calendarReturn}>Return to Calendar</button> </div>
-        <div style= {{margin: '10px'}}> <button className= 'detailBtn' onClick={handleDeleteEvent} style={{ marginLeft: "10px" }}> 
-          Event lame, plz remove
-        </button> </div>
-        <div style={{margin: '10px'}}> <button className='detailBtn' onClick={handleUpdateEvent} style={{ marginLeft: '10px' }}>
-          Update Event
-        </button> </div>
+      <div id="wrap">
+        <ul>
+          <h1
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              color: "salmon",
+            }}
+          >
+            Details
+          </h1>
+
+          <label style={{ width: "20%" }} className="addWrap">
+            id: {item.id}
+          </label>
+          <input
+            className="addWrap"
+            type="text"
+            placeholder={item.title}
+            value={editDetails.title}
+            onChange={(e) =>
+              setEditDetails({ ...editDetails, title: e.target.value })
+            }
+          />
+
+          <input
+            className="addWrap"
+            type="text"
+            placeholder={item.type}
+            value={editDetails.type}
+            onChange={(e) =>
+              setEditDetails({ ...editDetails, type: e.target.value })
+            }
+          />
+
+          <input
+            className="addWrap"
+            type="text"
+            placeholder={item.description}
+            value={editDetails.description}
+            onChange={(e) =>
+              setEditDetails({ ...editDetails, description: e.target.value })
+            }
+          />
+
+          <DatePicker
+            className="addWrap"
+            placeholderText={item.start}
+            showTimeSelect
+            style={{ marginRight: "10px" }}
+            selected={editDetails.start}
+            onChange={(start) => setEditDetails({ ...editDetails, start })}
+          />
+
+          <DatePicker
+            className="addWrap"
+            placeholderText={item.end}
+            showTimeSelect
+            style={{ marginRight: "10px" }}
+            selected={editDetails.end}
+            onChange={(end) => setEditDetails({ ...editDetails, end })}
+          />
+
+          <input
+            className="addWrap"
+            type="integer"
+            // placeholder={item.fundRequired}
+            defaultValue={item.fundRequired}
+            // value={editDetails.fundRequired}
+            onChange={(e) =>
+              setEditDetails({ ...editDetails, fundRequired: e.target.value })
+            }
+          />
+
+          <input
+            className="addWrap"
+            type="integer"
+            // placeholder={item.volunteerNeeded}
+            defaultValue={item.volunteerNeeded}
+            // value={editDetails.volunteerNeeded}
+            onChange={(e) =>
+              setEditDetails({
+                ...editDetails,
+                volunteerNeeded: e.target.value,
+              })
+            }
+          />
+
+          <input
+            className="addWrap"
+            type="integer"
+            // placeholder={item.userId}
+            defaultValue={item.userId}
+            // value={editDetails.userId}
+            onChange={(e) =>
+              setEditDetails({ ...editDetails, userId: e.target.value })
+            }
+          />
+        </ul>
+        <div className='buttonLayout'>
+        <div>
+          <button className="detailBtn" onClick={calendarReturn}>
+            Return to Calendar
+          </button>
         </div>
+        <div>
+          <button className="detailBtn" onClick={handleDeleteEvent}>
+            Event lame, plz remove
+          </button>
+        </div>
+        <div>
+          <button className="detailBtn" onClick={handleUpdateEvent}>
+            Update Event
+          </button>
+        </div>
+        </div>
+      </div>
     </container>
   );
 };
