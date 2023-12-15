74f7fe17cd44bd18be7246d190217167bb83a521
syboy25 <saed.herring25@gmail.com>
2023-12-15 13:53:49 -0500

    Edits made to Details.js able to update the events, need it to work with date picker

--- boostersphere/src/Details/Details.js
+++ boostersphere/src/Details/Details.js
@@ -1,9 +1,16 @@
+import { useEffect, useState } from "react";
 import "./Details.css";
 import { useNavigate, useParams } from "react-router-dom";
 
 export const Details = ({ item }) => {
   const { id } = useParams();
   const navigate = useNavigate();
+
+  const [editedItem, setEditedItem] = useState({});
+
+  useEffect(() => {
+    setEditedItem(item);
+  }, [item]);
 
   function handleDeleteEvent() {
     // Fetch to sevrver but it is not getting the request
@@ -12,22 +19,39 @@ export const Details = ({ item }) => {
     }).then(navigate(`/events`));
   }
 
+    // Function to update the event details
   const handleUpdateEvent = () => {
-  
+ 
     fetch(`http://localhost:8080/events/${id}`, {
-      method: "PATCH",
+      method: "PATCH", // Using the PATCH method to update
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(item),
+      body: JSON.stringify(editedItem), // Sending the edited data as JSON to the server
     })
       .then(() => {
         console.log("Event updated successfully");
       })
+
+      // Fetch the updated event data after a successful update
+      fetch(`http://localhost:8080/events/${id}`)
+        .then((response) => response.json())
+        .then((updatedEvenData) => {
+
+          // Update the state with the newly fetched data
+          setEditedItem(updatedEvenData);
+        })
       .catch((error) => {
         console.error("Error updating event", error);
       });
   };
+
+  // Function to handle changes in the input fields
+  const handleInputChange = (e) => {
+    const {name, value} = e.target; // Extracting name and value from the input field
+    setEditedItem({...editedItem, [name]: value}); // Updating the corresponding field in editedItem
+  
+  }
 
   const calendarReturn = () => {
     navigate(`/events`);
@@ -38,7 +62,31 @@ export const Details = ({ item }) => {
   return (
     <container>
   <div id='wrap'>
-    <ul>
+
+    <form style={{display: 'flex', justifyContent: 'center', color: 'salmon'}}>
+      <label>Event ID 
+      <input className='addwrap' type="text" name="id" value={editedItem.id || ""} readOnly ></input>
+      </label>
+      <label>Title:  
+      <input className='addwrap' type="text" name="title" value={editedItem.title || ""} onChange={handleInputChange} ></input>
+      </label>
+      <label>Description:  
+      <input className='addwrap' type="text" name="description" value={editedItem.description || ""} onChange={handleInputChange} ></input>
+      </label>
+      <label>Start:  
+      <input className='addwrap' type="text" name="start" value={editedItem.start || ""} onChange={handleInputChange} ></input>
+      </label>
+      <label>
+      <input className='addwrap' type="text" name="end" value={editedItem.end || ""} onChange={handleInputChange} ></input>
+      </label>
+      <label>Funds Required:  
+      <input className='addwrap' type="text" name="fundRequired" value={editedItem.fundRequired || ""} onChange={handleInputChange} ></input>
+      </label>
+      <label>Volunteers Needed: 
+      <input className='addwrap' type="text" name="volunteers" value={editedItem.volunteerNeeded || ""} onChange={handleInputChange} ></input>
+      </label>
+    </form>
+    {/* <ul>
       <h1 style={{display: 'flex', justifyContent: 'center', color: 'salmon'}}>Details</h1>
       <li className='addWrap'>{item.id}</li>
       <li className='addWrap'>{item.title}</li>
@@ -47,7 +95,7 @@ export const Details = ({ item }) => {
       <li className='addWrap'>{item.end}</li>
       <li className='addWrap'>{item.fundRequired}</li>
       <li className='addWrap'>{item.volunteerNeeded}</li>
-    </ul>
+    </ul> */}
   </div>  
       <div id='btns'>
         <button className= 'detailBtn' onClick={calendarReturn}>Return to Calendar</button>
