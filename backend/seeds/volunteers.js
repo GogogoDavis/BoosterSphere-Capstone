05828f4052ccf4c9633027e6a649c5c6bed5de8f
trevatheruiner <trevor.ml.jones@gmail.com>
2023-12-18 11:23:10 -0700

    created home/visitor page with full CSS and fetches from backend for Dashboard

--- backend/seeds/volunteers.js
+++ backend/seeds/volunteers.js
@@ -1,11 +1,11 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('volunteers').del()
   await knex('volunteers').insert([
-    {firstName: 'joel', lastName: 'dimpler', email:'helpfulperson@gmail.com', phone:'123-456-7890', event_id: 1}
+    {firstName: 'Joel', lastName: 'Dimpler', email:'helpfulperson@gmail.com', phone:'123-456-7890', event_id: 1}
   ]);
 };
