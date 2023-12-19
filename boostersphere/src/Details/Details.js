a1c0dd5fe13718b788066f4fca079afa2b459ba6
syboy25 <saed.herring25@gmail.com>
2023-12-19 14:40:08 -0500

    Edits to details css

--- boostersphere/src/Details/Details.js
+++ boostersphere/src/Details/Details.js
@@ -3,6 +3,8 @@ import { useNavigate, useParams } from "react-router-dom";
 import DatePicker from "react-datepicker";
 import React, {useState} from 'react';
 import mopey from '../DaMopester-nobackground.png'
+import { Sidebar } from '../Sidebar/Sidebar';
+
 
 export const Details = ({ item }) => {
   const { id } = useParams();
@@ -55,9 +57,9 @@ export const Details = ({ item }) => {
 
   return (
     <container id='detailscontainer'>
-       <div className='mopeyImg'>
+       {/* <div className='mopeyImg'>
             <img src={mopey} alt=''></img>
-              </div>
+              </div> */}
       <div id="wrap">
         <ul>
           <h1
@@ -67,112 +69,165 @@ export const Details = ({ item }) => {
             //   // justifyContent: "center",
             //   // color: "salmon",
             // }}
-          >Details</h1>
-          
-          <label className="event_label">Event ID: </label>
-          <input
-          className="detail_value"
-          value= {item.id}
-          readOnly
-          />
-          
+          > Event Details</h1>
+
+         <div className="div_row"> 
+          <div className="detail_container2">
+
+          <div className="div_label"><label className="event_label">Event ID: </label></div>
+
+          <div className="div_input">
+            <input
+            className="detail_value"
+            value= {item.id}
+            readOnly
+            />
+          </div>
+          </div>
         
           
+          <div className="div_row"> 
 
-          <label  className="event_label">Event Title: </label>
-          <input
-            className="detail_value"
-            type="text"
-            placeholder={item.title}
-            value={editDetails.title}
-            onChange={(e) =>
-              setEditDetails({ ...editDetails, title: e.target.value })
-            }
-          />
+            <div className="div_label"><label  className="event_label">Event Title: </label></div>
 
-          <label className="event_label">Party Type: </label>
-          <input
-            className="detail_value"
-            type="text"
-            placeholder={item.type}
-            value={editDetails.type}
-            onChange={(e) =>
-              setEditDetails({ ...editDetails, type: e.target.value })
-            }
-          />
+            <div className="div_input">
+            <input
+              className="detail_value"
+              type="text"
+              placeholder={item.title}
+              value={editDetails.title}
+              onChange={(e) =>
+                setEditDetails({ ...editDetails, title: e.target.value })
+              }
+            />
+            </div>
+          </div>
 
-          <label className="event_label">Event Details: </label>
-          <input
-            className="detail_value"
-            type="text"
-            placeholder={item.description}
-            value={editDetails.description}
-            style={{ height: 'auto', width: '800px', padding: '50px', margin: '10px' }}
-            onChange={(e) =>
-              setEditDetails({ ...editDetails, description: e.target.value })
-            }
-          />
-<div>     
-          <label className="event_label">Start Date: </label>
-          <DatePicker
-            className="detail_value"
-            placeholderText={item.start}
-            showTimeSelect
-            style={{ marginRight: "10px" }}
-            selected={editDetails.start}
-            onChange={(start) => setEditDetails({ ...editDetails, start })}
-          />
-</div>
-<div>
-          <label className="event_label">End Date: </label>
-          <DatePicker
-            className="detail_value"
-            placeholderText={item.end}
-            showTimeSelect
-            style={{ marginRight: "10px" }}
-            selected={editDetails.end}
-            onChange={(end) => setEditDetails({ ...editDetails, end })}
-          />
-</div>
+          <div className="div_row"> 
 
-          <label className="event_label">Amount of Funds Required: </label>
-          <input
-            className="detail_value"
-            type="integer"
-            placeholder={item.fundRequired}
-            // defaultValue={item.fundRequired}
-            // value={editDetails.fundRequired}
-            onChange={(e) =>
-              setEditDetails({ ...editDetails, fundRequired: e.target.value })
-            }
-          />
+            <div className="div_label"><label className="event_label">Party Type: </label></div>
 
-          <label className="event_label">Number of Volunteers Needed:</label>
-          <input
-            className="detail_value"
-            type="integer"
-            placeholder={item.volunteerNeeded}
-            // defaultValue={item.volunteerNeeded}
-            // value={editDetails.volunteerNeeded}
-            onChange={(e) =>
-              setEditDetails({
-                ...editDetails,
-                volunteerNeeded: e.target.value,
-              })
-            }
-          />
+            <div className="div_input">
+              <input
+                className="detail_value"
+                type="text"
+                placeholder={item.type}
+                value={editDetails.type}
+                onChange={(e) =>
+                  setEditDetails({ ...editDetails, type: e.target.value })
+                }
+              />
+            </div>
+          </div>
 
-          <label className="event_label">User ID: </label>
-          <input
-            className="detail_value"
-            type="integer"
-            placeholder={item.userId}
-            // defaultValue={item.userId}
-            // value={editDetails.userId}
-            onChange={(e) =>
-              setEditDetails({ ...editDetails, userId: e.target.value })
-            }
-          />
+          <div className="div_row"> 
+
+            <div className="div_label"><label className="event_label">Event Details: </label></div>
+
+            <div className="div_input">
+              <input
+                className="detail_value"
+                type="text"
+                placeholder={item.description}
+                value={editDetails.description}
+                style={{ height: 'auto', width: '800px', padding: '50px', margin: '10px' }}
+                onChange={(e) =>
+                  setEditDetails({ ...editDetails, description: e.target.value })
+                }
+              />
+            </div>
+          </div>
+
+          <div className="div_row"> 
+     
+          <div className="div_label"><label className="event_label">Start Date: </label></div>
+
+          <div className="div_input">
+            <DatePicker
+              className="detail_value"
+              placeholderText={item.start}
+              showTimeSelect
+              style={{ marginRight: "10px" }}
+              selected={editDetails.start}
+              onChange={(start) => setEditDetails({ ...editDetails, start })}
+            />
+          </div>
+      </div>
+      
+      <div className="div_row"> 
+
+          <div className="div_label"><label className="event_label">End Date: </label></div>
+
+          <div className="div_input">
+            <DatePicker
+              className="detail_value"
+              placeholderText={item.end}
+              showTimeSelect
+              style={{ marginRight: "10px" }}
+              selected={editDetails.end}
+              onChange={(end) => setEditDetails({ ...editDetails, end })}
+            />
+          </div>
+      </div>
+
+      <div className="div_row"> 
+
+            <div className="div_label"><label className="event_label">Amount of Funds Required: </label></div>
+
+            <div className="div_input">
+              <input
+                className="detail_value"
+                type="integer"
+                placeholder={item.fundRequired}
+                // defaultValue={item.fundRequired}
+                // value={editDetails.fundRequired}
+                onChange={(e) =>
+                  setEditDetails({ ...editDetails, fundRequired: e.target.value })
+                }
+              />
+            </div>
+          </div>
+
+          <div className="div_row"> 
+
+            <div className="div_label"><label className="event_label">Number of Volunteers Needed:</label></div>
+
+            <div className="div_input">
+              <input
+                className="detail_value"
+                type="integer"
+                placeholder={item.volunteerNeeded}
+                // defaultValue={item.volunteerNeeded}
+                // value={editDetails.volunteerNeeded}
+                onChange={(e) =>
+                  setEditDetails({
+                    ...editDetails,
+                    volunteerNeeded: e.target.value,
+                  })
+                }
+              />
+            </div>
+          </div>
+
+          <div className="div_row"> 
+
+            <div className="div_label"><label className="event_label">User ID: </label></div>
+            
+            <div className="div_input">
+              <input
+                className="detail_value"
+                type="integer"
+                placeholder={item.userId}
+                // defaultValue={item.userId}
+                // value={editDetails.userId}
+                onChange={(e) =>
+                  setEditDetails({ ...editDetails, userId: e.target.value })
+                }
+              />
+            </div>
+
+          </div>
+          </div>
         </ul>
         <div className='buttonLayout'>
           <button className="detailBtn" onClick={calendarReturn}>
@@ -187,6 +242,8 @@ export const Details = ({ item }) => {
         </div>
       </div>
       <div className='detail_wrapper'>
+      </div>
+      <div className='detail_wrapper2'>
       </div>
     </container>
     
