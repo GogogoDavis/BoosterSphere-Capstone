e36cbc69adf8a2ec026186fce0370f374a39135e
trevatheruiner <trevor.ml.jones@gmail.com>
2023-12-19 15:11:44 -0700

    new shop with space force patches instead

---
+++ backend/migrations/20231219210801_shop.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('shop', table => {
+    table.increments('id');
+    table.string('title');
+    table.string('image_path');
+    table.float('price');
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('shop')
+};
