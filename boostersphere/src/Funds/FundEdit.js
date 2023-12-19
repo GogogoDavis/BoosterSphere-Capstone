925d832e2a9a2244ab600c2b92e36ae3eefac062
dpark19 <d.park.official@gmail.com>
2023-12-19 11:03:32 -0500

    funds functionality

--- boostersphere/src/Funds/FundEdit.js
+++ boostersphere/src/Funds/FundEdit.js
@@ -4,6 +4,12 @@ import { Sidebar } from '../Sidebar/Sidebar';
 import { RiFundsBoxFill } from 'react-icons/ri';
 import { inputAdornmentClasses, responsiveFontSizes } from '@mui/material';
 
+//icons
+import { MdDelete } from "react-icons/md"
+import { FaRegEdit } from "react-icons/fa"
+import { BsClockHistory } from "react-icons/bs"
+import { IoIosExit } from "react-icons/io"
+
 import {
   LinearProgress,
   Button,
@@ -21,7 +27,9 @@ export const FundEdit = () => {
 
   const [formData, setFormData] = useState({ firstName: '', lastName: '', email: '', phone: '', event_id: NaN })
   const [formShowing, setFormShowing] = useState(false)
+  const [formShowingHistory, setFormShowingHistory] = useState(false)
   const [selectedEvent, setSelectedEvent] = useState()
+  const [fundsHistory, setFundsHistory] = useState()
 
   //inputs
   const [inputTitle, setinputTitle] = useState('')
@@ -38,6 +46,12 @@ export const FundEdit = () => {
     fetch('http://localhost:8080/funds')
       .then(res => res.json())
       .then(data => setFunds(data))
+  }, [toggleRefresh])
+
+  useEffect(() => {
+    fetch('http://localhost:8080/funds/transaction')
+      .then(res => res.json())
+      .then(data => setFundsHistory(data))
   }, [toggleRefresh])
 
 
@@ -87,10 +101,15 @@ export const FundEdit = () => {
     setFormShowing(true)
   }
 
+  const showFormHistory = (event) => {
+    setSelectedEvent(event)
+    setFormShowingHistory(true)
+  }
+
 
   const handleFundAdd = (selected) =>{
     console.log(selected.currRaised)
-    let total = (selected.currRaised == undefined) ? 0 + parseInt(moneytoAdd) : parseInt(selected.currRaised) + parseInt(moneytoAdd)
+    let total = (selected.currRaised == undefined) ? 0 + parseFloat(moneytoAdd) : parseFloat(selected.currRaised) + parseFloat(moneytoAdd)
 
     fetch('http://localhost:8080/funds', {
       method: 'PATCH',
@@ -100,6 +119,19 @@ export const FundEdit = () => {
         currRaised: total,
       })
     })
+
+
+    fetch('http://localhost:8080/funds/transaction', {
+      method: 'POST',
+      headers: {'content-type': 'application/json'},
+      body: JSON.stringify({
+        title: selected.title,
+        amount: moneytoAdd,
+        event_id: selected.id,
+        status: "add"
+      })
+    })
+
     setmoneytoAdd(0)
     setFormShowing(false)
     setTimeout(() => { settoggleRefresh(!toggleRefresh) }, 100)
@@ -108,7 +140,7 @@ export const FundEdit = () => {
 
   const handleFundDelete = (selected) =>{
     console.log(selected.currRaised)
-    let total = (selected.currRaised == undefined) ? 0 - parseInt(moneytoAdd) : parseInt(selected.currRaised) - parseInt(moneytoAdd)
+    let total = (selected.currRaised == undefined) ? 0 - parseFloat(moneytoAdd) : parseFloat(selected.currRaised) - parseFloat(moneytoAdd)
 
     fetch('http://localhost:8080/funds', {
       method: 'PATCH',
@@ -118,12 +150,74 @@ export const FundEdit = () => {
         currRaised: total,
       })
     })
+
+    fetch('http://localhost:8080/funds/transaction', {
+      method: 'POST',
+      headers: {'content-type': 'application/json'},
+      body: JSON.stringify({
+        title: selected.title,
+        amount: moneytoAdd,
+        event_id: selected.id,
+        status: "subtract"
+      })
+    })
+
     setmoneytoAdd(0)
     setFormShowing(false)
     setTimeout(() => { settoggleRefresh(!toggleRefresh) }, 100)
   }
-  
 
+
+
+  const deleteTransactions = (selected, elem) =>{
+    fetch(`http://localhost:8080/funds/transaction/${elem.id}`, {
+      method: 'DELETE',
+    })
+
+
+    if(elem.status == "subtract"){
+    let addtotal = (selected.currRaised == undefined) ? 0 + parseFloat(elem.amount) : parseFloat(selected.currRaised) + parseFloat(elem.amount)
+
+    fetch('http://localhost:8080/funds', {
+      method: 'PATCH',
+      headers: {'content-type': 'application/json'},
+      body: JSON.stringify({
+        id: selected.id,
+        currRaised: addtotal,
+      })
+    })
+  }
+
+
+
+  if(elem.status == "add"){
+    let subtotal = (selected.currRaised == undefined) ? 0 - parseFloat(elem.amount) : parseFloat(selected.currRaised) - parseFloat(elem.amount)
+
+    fetch('http://localhost:8080/funds', {
+      method: 'PATCH',
+      headers: {'content-type': 'application/json'},
+      body: JSON.stringify({
+        id: selected.id,
+        currRaised: subtotal,
+      })
+    })
+  }
+
+
+
+
+console.log(selectedEvent)
+
+    setTimeout(() => { settoggleRefresh(!toggleRefresh) }, 100)
+  }
+
+
+
+
+
+
+  
+console.log(fundsHistory)
 
   return !funds ? null : ((
     <>
@@ -133,26 +227,24 @@ export const FundEdit = () => {
 
         <div className='Funds_main'>
 
-          <div className='Funds_add'>
-            <input type='text' placeholder='Event Title' onChange={(e) => { setinputTitle(e.target.value) }} value={inputTitle} />
-            <input type='text' placeholder='Details' onChange={(e) => { setInputDetails(e.target.value) }} value={inputDetails} />
-            <input type='number' placeholder='Amount' onChange={(e) => { setinputAmount(e.target.value) }} value={inputAmount} />
-            <input type='number' placeholder='Amount' onChange={(e) => { setcurrRaised(e.target.value) }} value={currRaised} />
-            <button onClick={() => { addFund() }}>Add Fundraiser</button>
-          </div>
 
-
+        <div className='Funds_carddiv'>
           {funds.map((elem, index) => {
             return (
               <div className='Funds_card'>
                 <div className='Funds_Card_Container'>
 
-                  <h4>{elem.title}</h4>
+                  <div className='Funds_titleandbuttons'><h4>{elem.title}</h4>
+                    <div>                
+                      <button onClick={() => { showFormHistory(elem) }} className='Funds_deleteEditbtn'><BsClockHistory /></button>
+                      <button onClick={() => showForm(elem)} className='Funds_deleteEditbtn'><FaRegEdit /></button>
+                      <button onClick={() => { handleDelete(elem.id) }} className='Funds_deleteEditbtn'><MdDelete /></button>
+                      </div>
+                    </div>
                   <p>{elem.details}</p>
                   <p>Currently raised: ${elem.currRaised == undefined ? 0 : elem.currRaised}</p> <p>Amount Needed: ${elem.amount}</p>
 
-                  <button onClick={() => { handleDelete(elem.id) }}>Delete</button>
-                  <button onClick={() => showForm(elem)}>Edit Fund</button>
+
 
 
                   <LinearProgress
@@ -172,22 +264,59 @@ export const FundEdit = () => {
               </div>
             )
           })}
+          </div>
+
+          
+          <div className='Funds_add'>
+            <input type='text' placeholder='Event Title' onChange={(e) => { setinputTitle(e.target.value) }} value={inputTitle} />
+            <input type='text' placeholder='Details' onChange={(e) => { setInputDetails(e.target.value) }} value={inputDetails} />
+            <input type='number' placeholder='Amount' onChange={(e) => { setinputAmount(e.target.value) }} value={inputAmount} />
+            <input type='number' placeholder='Amount' onChange={(e) => { setcurrRaised(e.target.value) }} value={currRaised} />
+            <button onClick={() => { addFund() }}>Add Fundraiser</button>
+          </div>
 
         </div>
 
 
         {formShowing && (
-          <div className="volunteer-overlay">
-            <div className="volunteer-form-container form-names-span">
-              {selectedEvent.title}
-              {selectedEvent.currRaised}
+          <div className="funds_volunteer-overlay">
+            <div className="funds_volunteer-form-container funds_form-names-span">
+              <h3>{selectedEvent.title}</h3>
+              <p>Currently Raised: ${selectedEvent.currRaised}</p>
               <input placeholder="Amount" onChange={(e) => { setmoneytoAdd(e.target.value) }} value={moneytoAdd}/>
-              <button className="volunteer-button volunteer-button-form" onClick={() => setFormShowing(false)}>Cancel</button>
-              <button className="volunteer-button volunteer-button-form"  onClick={()=>{handleFundAdd(selectedEvent)}}>Add</button>
-              <button className="volunteer-button volunteer-button-form"  onClick={()=>{handleFundDelete(selectedEvent)}}>Subtract</button>
+              <button className="funds_volunteer-button volunteer-button-form" onClick={() => setFormShowing(false)}>Cancel</button>
+              <button className="funds_volunteer-button volunteer-button-form"  onClick={()=>{handleFundAdd(selectedEvent)}}>Add</button>
+              <button className="funds_volunteer-button volunteer-button-form"  onClick={()=>{handleFundDelete(selectedEvent)}}>Subtract</button>
             </div>
           </div>
         )}
+        
+
+
+        {formShowingHistory && (
+          <div className="funds_volunteer-overlay">
+            <div className="funds_volunteer-form-container funds_form-names-span">
+
+              <div className='funds_titleexit'>
+                <h3>{selectedEvent.title} Transaction History</h3>
+                <button className="funds_exitbutton" onClick={() => setFormShowingHistory(false)}><IoIosExit /></button>
+              </div>
+               
+
+              <ul>
+              {fundsHistory.filter(elem => elem.event_id == selectedEvent.id).map((elem, index) => {return <li className="funds_transactionli" style={{ background: elem.status == "add" ? "lightgreen" : "pink"}}>${elem.amount}<button onClick={()=>{deleteTransactions(selectedEvent, elem)}}>delete</button></li>})}
+              </ul>
+
+
+
+
+            </div>
+          </div>
+        )}
+
+
+
+
       </div>
     </>
   ));
