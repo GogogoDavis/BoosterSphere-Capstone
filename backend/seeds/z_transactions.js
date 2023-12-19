d88eb9f6d2ada49c6f5bade1029e9d6a292e5fd6
dpark19 <d.park.official@gmail.com>
2023-12-19 15:18:08 -0500

    quick seed change

--- backend/seeds/z_transactions.js
+++ backend/seeds/z_transactions.js
@@ -6,7 +6,6 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('transaction').del()
   await knex('transaction').insert([
-    {id: 1, title: 'bowling', amount: 200, event_id: 1, status: 'add'},
 
   ]);
 };
